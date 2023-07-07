104134422e7a3f21d704c86d0f47d0a228302077
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-07 01:19:14 -0600

    implement unit test 'create a empty optiona' spec

--- lib/src/test/groovy/optionals/chapter2/OptionalObjectsSpec.groovy
+++ lib/src/test/groovy/optionals/chapter2/OptionalObjectsSpec.groovy
@@ -1,6 +1,20 @@
 package optionals.chapter2
 
+import spock.lang.Shared
 import spock.lang.Specification
 
-class OptionalObjectsSpec extends Specification{
+class OptionalObjectsSpec extends Specification {
+
+  @Shared
+    instance = new OptionalObjects()
+
+  def "create a empty optional"() {
+    given:
+    Optional<String> response
+    when:
+    response = instance.createAEmptyOptional()
+    then:
+    response.isEmpty()
+  }
+
 }
