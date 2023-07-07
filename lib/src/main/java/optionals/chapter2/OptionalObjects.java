9f4643db6efa88a70ececc511c1129c32227ad75
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-07 01:18:42 -0600

    implement method createAEmptyOptional() and return a Optional<String> empty

--- lib/src/main/java/optionals/chapter2/OptionalObjects.java
+++ lib/src/main/java/optionals/chapter2/OptionalObjects.java
@@ -1,4 +1,12 @@
 package optionals.chapter2;
 
+import java.util.Optional;
+
 public class OptionalObjects {
+
+  public Optional<String> createAEmptyOptional(){
+    Optional<String> empty = Optional.empty();
+    return empty;
+  }
+
 }
