7322a61f08f52f27370c8d5d512e83337078d37e
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-07 01:27:09 -0600

    implement method throwErrorOnCreate() and return a NullPointerException

--- lib/src/main/java/optionals/chapter2/OptionalObjects.java
+++ lib/src/main/java/optionals/chapter2/OptionalObjects.java
@@ -4,9 +4,15 @@ import java.util.Optional;
 
 public class OptionalObjects {
 
-  public Optional<String> createAEmptyOptional(){
+  public Optional<String> createAEmptyOptional() {
     Optional<String> empty = Optional.empty();
     return empty;
   }
 
+  public Optional<String> throwErrorOnCreate() {
+    String string = null;
+    Optional<String> optionalNull = Optional.of(string);
+    return optionalNull;
+  }
 }
+
