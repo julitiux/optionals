28e4e1771f91dd5c81e2dd8332087420a6e51610
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-08 17:14:24 -0600

    implement method orElseWorks() where return a String name

--- lib/src/main/java/optionals/chapter5/DefaultValueWithOrElse.java
+++ lib/src/main/java/optionals/chapter5/DefaultValueWithOrElse.java
@@ -1,4 +1,13 @@
 package optionals.chapter5;
 
+import java.util.Optional;
+
 public class DefaultValueWithOrElse {
+
+  public String orElseWorks() {
+    String nullName = null;
+    String name = Optional.ofNullable(nullName).orElse("other string");
+    return name;
+  }
+
 }
