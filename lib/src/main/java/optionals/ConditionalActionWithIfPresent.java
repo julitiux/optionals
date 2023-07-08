ab077494f705e80fe066c2f2d5c39445e5d7e7da
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-08 17:05:22 -0600

    implement method withIfPresentWorks() using ifPresent from java.util.Optional

--- lib/src/main/java/optionals/ConditionalActionWithIfPresent.java
+++ lib/src/main/java/optionals/ConditionalActionWithIfPresent.java
@@ -1,6 +1,12 @@
 package optionals;
 
+import java.util.Optional;
+
 public class ConditionalActionWithIfPresent {
-  
+
+  public void withIfPresentWorks() {
+    Optional<String> optional = Optional.of("string");
+    optional.ifPresent(string -> System.out.println(string.length()));
+  }
 
 }
