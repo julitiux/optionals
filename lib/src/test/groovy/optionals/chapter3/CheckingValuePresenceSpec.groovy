4aa7f66ec16062f8e800adda22437447f68e4435
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-08 00:08:04 -0600

    implement unit test when is present

--- lib/src/test/groovy/optionals/chapter3/CheckingValuePresenceSpec.groovy
+++ lib/src/test/groovy/optionals/chapter3/CheckingValuePresenceSpec.groovy
@@ -1,7 +1,20 @@
 package optionals.chapter3
 
+import spock.lang.Shared
 import spock.lang.Specification
 
 class CheckingValuePresenceSpec extends Specification {
-  
+
+  @Shared
+    instance = new CheckingValuePresence()
+
+  def "when is present"() {
+    given:
+    boolean response
+    when:
+    response = instance.whenIsPresent()
+    then:
+    response
+  }
+
 }
