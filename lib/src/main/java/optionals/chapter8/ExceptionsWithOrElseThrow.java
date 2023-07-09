e8176afb73d114acc0f8e568623aa4129140465a
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-08 19:40:31 -0600

    create class ExceptionsWithOrElseThrow and implement method orElseThrowWorks()

---
+++ lib/src/main/java/optionals/chapter8/ExceptionsWithOrElseThrow.java
@@ -0,0 +1,12 @@
+package optionals.chapter8;
+
+import java.util.Optional;
+
+public class ExceptionsWithOrElseThrow {
+
+  public void orElseThrowWorks() {
+    String nullName = null;
+    Optional.ofNullable(nullName).orElseThrow(NullPointerException::new);
+  }
+
+}
