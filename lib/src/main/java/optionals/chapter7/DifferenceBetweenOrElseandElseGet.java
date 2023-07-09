9fb56c83158edfb349253149485e71c44faea72a
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-08 19:26:48 -0600

    implement method getMyDefault() and return a String

---
+++ lib/src/main/java/optionals/chapter7/DifferenceBetweenOrElseandElseGet.java
@@ -0,0 +1,10 @@
+package optionals.chapter7;
+
+public class DifferenceBetweenOrElseandElseGet {
+
+  public String getMyDefault() {
+    System.out.println("Getting Default Value");
+    return "Default Value";
+  }
+
+}
