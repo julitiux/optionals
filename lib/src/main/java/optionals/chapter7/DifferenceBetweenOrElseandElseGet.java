964022de0a3ff9878a66e003525303dd9f18f5f2
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-08 19:35:12 -0600

    modify the value string to 'Text present'

--- lib/src/main/java/optionals/chapter7/DifferenceBetweenOrElseandElseGet.java
+++ lib/src/main/java/optionals/chapter7/DifferenceBetweenOrElseandElseGet.java
@@ -10,13 +10,13 @@ public class DifferenceBetweenOrElseandElseGet {
   }
 
   public void orElseGet() {
-    String string = null;
+    String string = "Text present";
     String defaultString = Optional.ofNullable(string).orElseGet(DifferenceBetweenOrElseandElseGet::getMyDefault);
     System.out.println(defaultString);
   }
 
   public void orElse() {
-    String string = null;
+    String string = "Text present";
     String defaultString = Optional.ofNullable(string).orElse(getMyDefault());
     System.out.println(defaultString);
   }
