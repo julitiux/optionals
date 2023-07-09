ddcb124cc5e46e1d2b0d4a1edeadaf740e9c734d
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-08 19:30:01 -0600

    make static method getMyDefault(). Implement method orElseGet using Optiona...orElseGet

--- lib/src/main/java/optionals/chapter7/DifferenceBetweenOrElseandElseGet.java
+++ lib/src/main/java/optionals/chapter7/DifferenceBetweenOrElseandElseGet.java
@@ -1,10 +1,17 @@
 package optionals.chapter7;
 
+import java.util.Optional;
+
 public class DifferenceBetweenOrElseandElseGet {
 
-  public String getMyDefault() {
+  public static String getMyDefault() {
     System.out.println("Getting Default Value");
     return "Default Value";
+  }
+
+  public void orElseGet() {
+    String string = null;
+    String defaultString = Optional.ofNullable(string).orElseGet(DifferenceBetweenOrElseandElseGet::getMyDefault);
   }
 
 }
