6f2148ff8171ab294cbe2c16dfa123782290bae6
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-09 00:15:07 -0600

    implement a class Modem

---
+++ lib/src/main/java/optionals/chapter10/Modem.java
@@ -0,0 +1,17 @@
+package optionals.chapter10;
+
+public class Modem {
+
+  Double price;
+
+  public Modem(Double price) {
+    this.price = price;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+}
