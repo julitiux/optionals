3d4669d6f5c7937cd6ff10d051396fec1606a495
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-09 00:43:28 -0600

    modify the methid mapWorksCountElementsOfList an recive a list od String

--- lib/src/main/java/optionals/chapter11/TransformingValuesWithMap.java
+++ lib/src/main/java/optionals/chapter11/TransformingValuesWithMap.java
@@ -1,13 +1,11 @@
 package optionals.chapter11;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 public class TransformingValuesWithMap {
 
-  public Integer mapWorksCountElementsOfList() {
-    List<String> list = Arrays.asList("paypal", "oracle", "", "microsoft", "", "apple");
+  public Integer mapWorksCountElementsOfList(List<String> list) {
     Optional<List<String>> optionalList = Optional.of(list);
     return optionalList.map(List::size).orElse(0);
   }
