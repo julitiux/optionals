836bd8c95c0a93b5570d3c43bf9f75eb6b0bfbdc
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-10 00:20:47 -0600

    implement an unit test 'when FlatMap Is Not Here'

---
+++ lib/src/test/groovy/optionals/chapter11/PersonalWithMapSpec.groovy
@@ -0,0 +1,20 @@
+package optionals.chapter11
+
+import spock.lang.Shared
+import spock.lang.Specification
+
+class PersonalWithMapSpec extends Specification {
+
+  @Shared
+    instance = new PersonalWithMap()
+
+  def "when FlatMap Is Not Here"() {
+    when:
+    String response = instance.whenFlatMapIsNotHere(_Person)
+    then:
+    response == _response
+    where:
+    _Person                                           | _response
+    Optional.of(new Person("Julito", 10, "password")) | "Julito"
+  }
+}
